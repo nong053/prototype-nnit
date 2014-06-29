00a38b75b5524f5700d611177317fea5aaee9716
kosit.arom <kosit.arom@gmail.com>
2014-06-29 07:58:20 +0000

    UPDATE

--- .htaccess2.php
+++ .htaccess2.php
@@ -4,4 +4,6 @@
 RewriteEngine On
 RewriteRule ^([a-zA-Z0-9_-]+)$ user.php?username=$1
 RewriteRule ^([a-zA-Z0-9_-]+)/$ user.php?username=$1
+
+RewriteRule ^view/(.*).html /dispatcher.php?page=$1
 </IfModule>
--- index.php
+++ index.php
@@ -18,7 +18,11 @@ if(count($userAsUrl)>2){
 //echo "userAsUrl[1]=".$userAsUrl[0]."<br>";
 //echo "userAsUrl[2]=".$userAsUrl[1]."<br>";
 //echo "userAsUrl[3]=".$userAsUrl[2]."<br>";
-
+//when domain name is signin here if domain name 
+// or so embed sesssion and query form data base for  user  sush as
+// domain nn-it
+// embed url-user=nn-it
+//get query user nn-it for display now.
 
 if(($host!="www.nn-webready.com") and ($host!="nn-webready.com") and($host!="localhost:9999")){
 	//echo $host."!="."www.nn-it.com";
